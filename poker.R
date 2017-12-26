15832e09f2ac837aabf1c78bc58041c4befbc9ff
林明潔 <hebe091936@gmail.com>
2017-12-26 20:03:01 +0800

    Signed-off-by: 林明潔 <hebe091936@gmail.com>

--- poker.R
+++ poker.R
@@ -1,5 +1,7 @@
 number = c(1:13,1:13,1:13,1:13)
+number1=rep(1:13,4)
+identical(number,number1)
 suit = c(rep("黑桃",13),rep("紅心",13),rep("方塊",13),rep("梅花",13))
 cards = paste(suit,number)
+str(cardscards)
 sample(cards,5)
-[1] "方塊 4" "紅心 9" "紅心 4" "方塊 8" "梅花 12"
