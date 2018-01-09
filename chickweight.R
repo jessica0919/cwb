dc0f130b71fefed115355e8f8a1f300101bc1519
林明潔 <hebe091936@gmail.com>
2018-01-09 20:20:57 +0800

    Signed-off-by: 林明潔 <hebe091936@gmail.com>

--- chickweight.R
+++ chickweight.R
@@ -26,3 +26,6 @@ boxplot(ChickWeight$weight ~ ChickWeight$Diet)
 
 with(ChickWeight, plot(Time, weight, col = Diet,pch=20,ylab='Weight'))
 legend('topleft',legend=paste("Diet",levels(ChickWeight$Diet)),col=1:4,lwd=3)
+
+ChickWeight$Chick
+table(ChickWeight$Chick)
