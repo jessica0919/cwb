7cfd8e37253a2dff8c2f7be4888d8b98d518360b
林明潔 <hebe091936@gmail.com>
2018-01-09 20:22:57 +0800

    Signed-off-by: 林明潔 <hebe091936@gmail.com>

--- chickweight.R
+++ chickweight.R
@@ -28,4 +28,5 @@ with(ChickWeight, plot(Time, weight, col = Diet,pch=20,ylab='Weight'))
 legend('topleft',legend=paste("Diet",levels(ChickWeight$Diet)),col=1:4,lwd=3)
 
 ChickWeight$Chick
-table(ChickWeight$Chick)
+sort(table(ChickWeight$Chick))
+
